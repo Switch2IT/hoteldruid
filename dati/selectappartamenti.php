e85ae5eb8fa0b108612d7453b49e4b577c4f01f0
Guillaume Vandecasteele <guillaume.vandecasteele@gmail.com>
2017-05-30 15:23:43 +0200

    Installation files now truly added

---
+++ dati/selectappartamenti.php
@@ -0,0 +1,24 @@
+<?php 
+echo "
+<option value=\"01\">01</option>
+<option value=\"02\">02</option>
+<option value=\"03\">03</option>
+<option value=\"04\">04</option>
+<option value=\"05\">05</option>
+<option value=\"06\">06</option>
+<option value=\"07\">07</option>
+<option value=\"08\">08</option>
+<option value=\"09\">09</option>
+<option value=\"10\">10</option>
+<option value=\"11\">11</option>
+<option value=\"12\">12</option>
+<option value=\"13\">13</option>
+<option value=\"14\">14</option>
+<option value=\"15\">15</option>
+<option value=\"16a\">16a</option>
+<option value=\"16b\">16b</option>
+<option value=\"16c\">16c</option>
+<option value=\"16d\">16d</option>
+<option value=\"16e\">16e</option>
+"; 
+?>
